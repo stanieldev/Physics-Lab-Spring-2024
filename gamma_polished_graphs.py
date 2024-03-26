12731f0da18e4415c33490387815bb2ae1a43772
Stanley Goodwin <github@staniel.dev>
2024-03-25 20:53:08 -0400

    Updates

--- gamma_polished_graphs.py
+++ gamma_polished_graphs.py
@@ -1,9 +1,24 @@
 # Imports
 import numpy as np
 import matplotlib.pyplot as plt
+import lmfit as lm
 
 
 
+def sigmoid(x, amplitude, steepness, center, voffset):
+    return amplitude / (1 + np.exp(-steepness * (x - center))) + voffset
+
+def offset_gaussian(x, amplitude, center, sigma, voffset):
+    return amplitude * np.exp(-((x - center)**2) / (2 * sigma**2)) + voffset
+
+def gaussian(x, amplitude, center, sigma):
+    return amplitude * np.exp(-((x - center)**2) / (2 * sigma**2))
+
+
+
+
+
+# ElementData class
 class ElementData:
     def __init__(self, name, filename, skiprows=25):
         data = self.load_data(filename, skiprows)
@@ -24,94 +39,193 @@ class ElementData:
         plt.legend()
         plt.show()
 
-    def plot_energy(self, log=False):
+    def plot_energy(self, log=False, color="black"):
         if log: plt.yscale('log')
-        plt.scatter(self.energy, self.counts, label='Measured Data', color='black', marker='.')
+        plt.scatter(self.energy, self.counts, label='Measured Data', color=color, marker='.')
         plt.title(rf'MCA {self.name} : Counts vs Energy')
         plt.xlabel('Energy (keV)')
         plt.ylabel('Counts')
         plt.legend()
-        plt.show()
+
+    def plot_adjusted_energy(self, background: list, log=False, color="black"):
+        counts = self.counts.copy()
+        counts -= background
+        counts[counts < 1] = 0
+        if log: plt.yscale('log')
+        plt.scatter(self.energy, counts, label='Adjusted Measured Data', color=color, marker='.')
+        plt.title(rf'MCA {self.name} : Counts vs Energy')
+        plt.xlabel('Energy (keV)')
+        plt.ylabel('Counts')
+        plt.legend()
+
+    def find_compton_edge(self, min_energy, max_energy, p0):
+        
+        # Filter data
+        x_data = self.energy[(self.energy > min_energy) & (self.energy < max_energy)]
+        y_data = self.counts[(self.energy > min_energy) & (self.energy < max_energy)]
+
+        # Find the compton edge
+        model = lm.Model(sigmoid)
+        params = model.make_params(amplitude=p0[0], steepness=p0[1], center=p0[2], voffset=p0[3])
+        result = model.fit(y_data, params, x=x_data)
+        return result
+
+    def plot_compton_edge(self, min_energy, max_energy, p0, label="Compton Edge", color="green"):
+        
+        # Find the compton edge
+        result = self.find_compton_edge(min_energy, max_energy, p0)
+        print(result.fit_report())
+        
+        # Plot the compton edge
+        params = result.best_values
+        x_fit = np.linspace(min_energy, max_energy, 1000)
+        y_fit = sigmoid(x_fit, params['amplitude'], params['steepness'], params['center'], params['voffset'])
+        plt.plot(x_fit, y_fit, label=label, color=color)
+        plt.xlabel('Energy (keV)')
+        plt.ylabel('Counts')
+        plt.legend()
+
+    def find_backscatter_peak(self, min_energy, max_energy, p0):
+        
+        # Filter data
+        x_data = self.energy[(self.energy > min_energy) & (self.energy < max_energy)]
+        y_data = self.counts[(self.energy > min_energy) & (self.energy < max_energy)]
+
+        # Find the backscatter peak
+        model = lm.Model(offset_gaussian)
+        params = model.make_params(amplitude=p0[0], center=p0[1], sigma=p0[2], voffset=p0[3])
+        result = model.fit(y_data, params, x=x_data)
+        return result
+
+    def plot_backscatter_peak(self, min_energy, max_energy, p0, label="Backscatter Peak", color="red"):
+
+        # Find the backscatter peak
+        result = self.find_backscatter_peak(min_energy, max_energy, p0)
+        print(result.fit_report())
+        
+        # Plot the backscatter peak
+        params = result.best_values
+        x_fit = np.linspace(min_energy, max_energy, 1000)
+        y_fit = offset_gaussian(x_fit, params['amplitude'], params['center'], params['sigma'], params['voffset'])
+        plt.plot(x_fit, y_fit, label=label, color=color)
+        plt.xlabel('Energy (keV)')
+        plt.ylabel('Counts')
+        plt.legend()
+
+    def find_photopeak(self, min_energy, max_energy, p0):
+        
+        # Filter data
+        x_data = self.energy[(self.energy > min_energy) & (self.energy < max_energy)]
+        y_data = self.counts[(self.energy > min_energy) & (self.energy < max_energy)]
+
+        # Find the photopeak
+        
+        model = lm.Model(gaussian)
+        params = model.make_params(amplitude=p0[0], center=p0[1], sigma=p0[2])
+        result = model.fit(y_data, params, x=x_data)
+        return result
+    
+    def plot_photopeak(self, min_energy, max_energy, p0, label="Photopeak", color="blue"):
+        
+        # Find the photopeak
+        result = self.find_photopeak(min_energy, max_energy, p0)
+        print(result.fit_report())
+        
+        # Plot the photopeak
+        params = result.best_values
+        x_fit = np.linspace(min_energy, max_energy, 1000)
+        y_fit = gaussian(x_fit, params['amplitude'], params['center'], params['sigma'])
+        plt.plot(x_fit, y_fit, label=label, color=color)
+        plt.xlabel('Energy (keV)')
+        plt.ylabel('Counts')
+        plt.legend()
+
+    
+
+
+# Low Energy Background
+BG_LOW = ElementData("Low Energy Background", "gamma_spectroscopy/background_low/background.tsv")
+CS_137 = ElementData(r"$^{137}$Cs", "gamma_spectroscopy/caesium_137/caesium.tsv")
+BA_133 = ElementData(r"$^{133}$Ba", "gamma_spectroscopy/barium_133/barium_133.tsv")
+MN_54 = ElementData(r"$^{54}$Mn", "gamma_spectroscopy/manganese_54/manganese.tsv")
+
+# High Energy Background
+BG_HIGH = ElementData("High Energy Background", "gamma_spectroscopy/background_high/background.tsv")
+NA_22 = ElementData(r"$^{22}$Na", "gamma_spectroscopy/sodium_22/Na22.tsv", skiprows=26)
+CO_60 = ElementData(r"$^{60}$Co", "gamma_spectroscopy/cobalt_60/Co60.tsv", skiprows=26)
+SR_90 = ElementData(r"$^{90}$Sr", "gamma_spectroscopy/strontium_90/sr90.tsv")
+
+
 
 
 
 # Define the data
-BA_133 = ElementData(r"$^{133}$Ba", "gamma_spectroscopy/barium_133/barium_133.tsv")
 BA_137 = ElementData(r"$^{137}$Ba", "gamma_spectroscopy/barium_137/barium_137.tsv", skiprows=26)
-CS_137 = ElementData(r"$^{137}$Cs", "gamma_spectroscopy/caesium_137/caesium.tsv")
-CO_60 = ElementData(r"$^{60}$Co", "gamma_spectroscopy/cobalt_60/Co60.tsv", skiprows=26)
-MN_54 = ElementData(r"$^{54}$Mn", "gamma_spectroscopy/manganese_54/manganese.tsv")
-NA_22 = ElementData(r"$^{22}$Na", "gamma_spectroscopy/sodium_22/Na22.tsv", skiprows=26)
-SR_90 = ElementData(r"$^{90}$Sr", "gamma_spectroscopy/strontium_90/sr90.tsv")
 
-# Define the background data
-BG = ElementData("Background 1", "gamma_spectroscopy/background/background.tsv")
-BG_energy = BG.energy
-BG_counts = BG.counts
+
 
 
 
 # Plot Caesium 137
 if False:
-    background_counts = (1482.61/9144.33) * BG.counts
-    CS_137.plot_energy(log=True)
-    CS_137.counts -= background_counts
-    CS_137.counts[CS_137.counts < 1] = 0
-    CS_137.plot_energy(log=True)
+    CS_137.plot_adjusted_energy((1482.61/9144.33) * BG_LOW.counts, log=True)
+    CS_137.plot_backscatter_peak(180, 240, [10000, 200, 10, 5000])
+    CS_137.plot_compton_edge(400, 610, [4000, 0.1, 511, 1350])
+    CS_137.plot_photopeak(630, 750, [10000, 680, 10])
+    plt.show()
     
 # Plot Barium 133
 if False:
-    background_counts = (338.03/9144.33) * BG.counts
-    BA_133.plot_energy(log=True)
-    BA_133.counts -= background_counts
-    BA_133.counts[BA_133.counts < 1] = 0
-    BA_133.plot_energy(log=True)
+    BA_133.plot_adjusted_energy((338.03/9144.33) * BG_LOW.counts, log=True)
+    BA_133.plot_backscatter_peak(105, 141, [6000, 125, 10, 3600])
+    BA_133.plot_compton_edge(160, 260, [3000, 0.1, 213, 1200])
+    BA_133.plot_photopeak(340, 380, [9610, 365, 10])
+    plt.show()
 
 # Plot Mangenese 54
 if False:
-    background_counts = (518.05/9144.33) * BG.counts
-    MN_54.plot_energy(log=True)
-    MN_54.counts -= background_counts
-    MN_54.counts[MN_54.counts < 1] = 0
-    MN_54.plot_energy(log=True)
-
-
-
-# Adjusted Background to match the energy scale of the other data
-K = 21.6/11.1
-ADJ_BG = []
-for i in range(0, int(1024)):
-    bottom = int(i*K)
-    top = bottom + 1
-    R = i*K - bottom
-    try:
-        Y = BG.counts[bottom] * R + BG.counts[top] * (1-R)
-    except:
-        Y = 0
-    ADJ_BG.append(Y)
+    MN_54.plot_adjusted_energy((518.05/9144.33) * BG_LOW.counts, log=True)
+    MN_54.plot_backscatter_peak(190, 420, [150, 220, 10, 71])
+    MN_54.plot_compton_edge(590, 780, [100, 0.1, 690, 20])
+    MN_54.plot_photopeak(820, 960, [350, 881, 10])
+    plt.show()
 
 
 
 # Plot Sodium 22
 if False:
-    background_counts = (497.64/9144.33) * BG.counts
-    NA_22.plot_energy(log=True)
-    NA_22.counts -= ADJ_BG
-    NA_22.counts[NA_22.counts < 1] = 0
-    NA_22.plot_energy(log=True)
+    NA_22.plot_adjusted_energy((497.64/9144.33) * BG_HIGH.counts, log=True)
+    NA_22.plot_backscatter_peak(135, 276, [10000, 175, 10, 2400], label="Backscatter Peak 1")
+    NA_22.plot_compton_edge(280, 430, [10000, 0.1, 330, 2400], label="Compton Edge 1")
+    NA_22.plot_backscatter_peak(430, 550, [30000, 510, 10, 2400], label="Photopeak 1", color="blue")
+
+    NA_22.plot_backscatter_peak(635, 800, [600, 702, 10, 1200], label="Backscatter Peak 2")
+    NA_22.plot_compton_edge(980, 1200, [1200, 0.1, 1090, 300], label="Compton Edge 2")
+    NA_22.plot_backscatter_peak(1203, 1390, [1000, 1287, 10, 330], label="Photopeak 2", color="blue")
+
+
+    NA_22.plot_backscatter_peak(1720, 1930, [200, 1820, 10, 13], label="Photopeak 3", color="blue")
+    plt.show()
 
 # Plot Cobalt 60
 if False:
-    background_counts = (586.76/9144.33) * BG.counts
-    CO_60.plot_energy(log=True)
-    CO_60.counts -= ADJ_BG
-    CO_60.counts[CO_60.counts < 1] = 0
-    CO_60.plot_energy(log=True)
+    CO_60.plot_adjusted_energy((586.76/9144.33) * BG_HIGH.counts, log=True)
+
+    CO_60.plot_backscatter_peak(180, 300, [2500, 205, 10, 1400], label="Backscatter Peak 1")
+    CO_60.plot_compton_edge(900, 1030, [600, 0.1, 950, 1100], label="Compton Edge 1")
+    CO_60.plot_backscatter_peak(1120, 1250, [2500, 1174, 10, 900], label="Photopeak 1", color="blue")
+
+    CO_60.plot_backscatter_peak(1270, 1420, [2500, 1332, 10, 100], label="Backscatter Peak 2")
+    CO_60.plot_compton_edge(2100, 2380, [20, 2285, 10, 4], label="Compton Edge 2")
+    CO_60.plot_backscatter_peak(2370, 2610, [44, 2506, 10, 3], label="Photopeak 2", color="blue")
+
+    plt.show()
 
 # Plot Strontium 90
 if False:
-    background_counts = (5400.83/9144.33) * BG.counts
-    SR_90.plot_energy(log=False)
-    SR_90.counts -= ADJ_BG
-    SR_90.counts[SR_90.counts < 1] = 0
-    SR_90.plot_energy(log=False)
+    SR_90.plot_adjusted_energy((5400.83/9144.33) * BG_HIGH.counts, log=False)
+    plt.show()
+
+
+
+
--- xray_diffraction.py
+++ xray_diffraction.py
@@ -3,20 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
-
-
-SHOW_DATASET = True
+SHOW_DATASET = False
 if SHOW_DATASET:
 
     # Load the data
+    data_8kV = np.loadtxt('xray_diffraction/8KV.txt', delimiter='\t', skiprows=3)
+    data_10kV = np.loadtxt('xray_diffraction/10KV.txt', delimiter='\t', skiprows=3)
+    data_12kV = np.loadtxt('xray_diffraction/12KV.txt', delimiter='\t', skiprows=3)
     data_15kV = np.loadtxt('xray_diffraction/15KV.txt', delimiter='\t', skiprows=3)
     data_20kV = np.loadtxt('xray_diffraction/20KV.txt', delimiter='\t', skiprows=3)
     data_25kV = np.loadtxt('xray_diffraction/25KV.txt', delimiter='\t', skiprows=3)
     data_30kV = np.loadtxt('xray_diffraction/30KV.txt', delimiter='\t', skiprows=3)
-    data_35kV = np.loadtxt('xray_diffraction/35KV.txt', delimiter='\t', skiprows=3)
+    data_35kV = np.loadtxt('xray_diffraction/35KV_old.txt', delimiter='\t', skiprows=3)
 
     # Plot the data
+    plt.plot(data_8kV[:, 0], data_8kV[:, 1], label='8 kV')
+    plt.plot(data_10kV[:, 0], data_10kV[:, 1], label='10 kV')
+    plt.plot(data_12kV[:, 0], data_12kV[:, 1], label='12 kV')
     plt.plot(data_15kV[:, 0], data_15kV[:, 1], label='15 kV')
     plt.plot(data_20kV[:, 0], data_20kV[:, 1], label='20 kV')
     plt.plot(data_25kV[:, 0], data_25kV[:, 1], label='25 kV')
@@ -31,10 +34,7 @@ if SHOW_DATASET:
     plt.show()
 
 
-
-
-
-SHOW_NORMALIZED_DATASET = True
+SHOW_NORMALIZED_DATASET = False
 if SHOW_NORMALIZED_DATASET:
 
     # Load the data
@@ -42,7 +42,7 @@ if SHOW_NORMALIZED_DATASET:
     data_20kV = np.loadtxt('xray_diffraction/20KV.txt', delimiter='\t', skiprows=3)
     data_25kV = np.loadtxt('xray_diffraction/25KV.txt', delimiter='\t', skiprows=3)
     data_30kV = np.loadtxt('xray_diffraction/30KV.txt', delimiter='\t', skiprows=3)
-    data_35kV = np.loadtxt('xray_diffraction/35KV.txt', delimiter='\t', skiprows=3)
+    data_35kV = np.loadtxt('xray_diffraction/35KV_old.txt', delimiter='\t', skiprows=3)
 
     # Plot the data
     plt.plot(data_15kV[:, 0], data_15kV[:, 1]/(15-11.588), label='15 kV')
@@ -59,3 +59,20 @@ if SHOW_NORMALIZED_DATASET:
     plt.show()
 
 
+SHOW_NICKEL_FILTER = True
+if SHOW_NICKEL_FILTER:
+
+    # Load the data
+    data_35kV = np.loadtxt('xray_diffraction/35KV.txt', delimiter='\t', skiprows=3)
+    data_35kV_ni = np.loadtxt('xray_diffraction/35KV_Ni.txt', delimiter='\t', skiprows=3)
+
+    # Plot the data
+    plt.plot(data_35kV[:, 0], data_35kV[:, 1], label='Unfiltered')
+    plt.plot(data_35kV_ni[:, 0], data_35kV_ni[:, 1], label='Ni Filter')
+
+    # Set up the plot
+    plt.title('X-ray Diffraction : Intensity vs. Crystal Angle')
+    plt.xlabel('Crystal Angle θ (degrees)')
+    plt.ylabel('Intensity (a.u.)')
+    plt.legend()
+    plt.show()
